6344eea227b1dcb1a8ef9e059e26967e22ed03bd
Juan <juanpablo_1810@hotmail.com>
2022-01-28 20:23:05 -0500

    dashboard and not found pages

---
+++ src/pages/not-found.js
@@ -0,0 +1,11 @@
+import {useffect} from 'react';
+import Header from '../components/header';
+
+
+export default function NotFound(){
+	useEffect(())=>{
+		document.title = 'Not Found- Instagram';
+
+	}
+	
+}
