bfde2b63678b50f6777f551034f7c4d7a2268b54
Juan <juanpablo_1810@hotmail.com>
2022-01-30 23:22:57 -0500

    main

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -3,9 +3,17 @@ import Header from '../components/header';
 
 
 export default function NotFound(){
-	useEffect(())=>{
+	useEffect(()=>{
 		document.title = 'Not Found- Instagram';
 
-	}
-	
+	},[]);
+	return(
+		<div className="bg-gray-background">
+			<Header/>
+			<div className="mx-auto max-w-screen-lg">
+				<p className="text-center text-2x1">Not Found!</p>
+			</div>
+
+		</div>
+		);	
 }
